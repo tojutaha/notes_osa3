4749491653e90d05362850e03765466f21211bee
Tomi Halko <tomi.halko@gmail.com>
2023-09-07 12:56:15 +0300

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
+require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
+const Note = require("./models/note")
 
 const app = express()
 
@@ -32,33 +34,25 @@ app.get("/", (req, res) => {
 */
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes)
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
 })
 
 app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
+  Note.findById(req.params.id).then(note => {
     res.json(note)
-  } else {
-    res.status(404).end()
-  }
+  })
 })
 
+/*
 app.delete("/api/notes/:id", (req, res) => {
   const id = Number(req.params.id)
   notes = notes.filter(note => note.id !== id)
   
   res.status(204).end()
 })
-
-const generatedID = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+*/
 
 app.post("/api/notes/", (req, res) => {
   const body = req.body
@@ -68,18 +62,17 @@ app.post("/api/notes/", (req, res) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generatedID(),
-  }
+  })
 
-  notes = notes.concat(note)
-  console.log(note)
-  res.json(note)
+  note.save().then(savedNote => {
+    res.json(savedNote)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 }) 
